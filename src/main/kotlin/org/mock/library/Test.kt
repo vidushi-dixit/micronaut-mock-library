e11c19744c6f1e3a70a83d5887354b5a60d4eb63
vidushi-dixit <vidushi.dixit@medly.com>
2022-01-19 18:42:49 +0530

    injected singleton class

--- src/main/kotlin/org/mock/library/Test.kt
+++ src/main/kotlin/org/mock/library/Test.kt
@@ -1,9 +1,10 @@
 package org.mock.library
 
 import io.micronaut.context.annotation.EachProperty
+import javax.inject.Inject
 
 @EachProperty(Test.PREFIX)
-class Test() {
+class Test(@Inject private val singletonClass: SingletonClass) {
 
     private var url: String? = null
     fun setUrl(url: String?) {
